7f3267f2f77c10a4a68e677224e1acb47a5d0054
Your Name <kalaitzidispaul@gmail.com>
2022-03-31 21:25:08 -0400

    added app.get req

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -12,3 +12,30 @@ app.use(express.urlencoded({ extended: true }));
 
 //a middleware that drills in and allows access for style sheet and htmls
 app.use(express.static('public'));
+
+//get requests 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+
+//create new note function
+
+
+
+
+
+//delete note function 
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
